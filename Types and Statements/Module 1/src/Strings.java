5b7a559d9acc243d031f1444dbc79b6daaa1bb69
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-12-07 12:20:17 -0600

    add string practice

---
+++ Types and Statements/Module 1/src/Strings.java
@@ -0,0 +1,8 @@
+public class Strings {
+    public static void main(String[] args) {
+        System.out.println("Hello strings");
+
+        String message = "baked apples";
+        System.out.println(message.charAt(message.length() - 1));
+    }
+}
