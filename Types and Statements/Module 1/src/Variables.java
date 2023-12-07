cc80442254f1653c35b1ec3499c5b00705cdfead
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-12-07 11:59:32 -0600

    add variable practice

---
+++ Types and Statements/Module 1/src/Variables.java
@@ -0,0 +1,26 @@
+public class Variables {
+    public static void main(String[] args) {
+        int dogCount = 94;
+        int catCount = 112;
+        int petCount = dogCount + catCount;
+//        System.out.println(petCount);
+
+        byte b = 56;
+        short s = 1025;
+        int i = 314;
+        long lng = -256;
+
+        float f = 3;
+        int g = 3;
+        System.out.println(f == g);
+//        System.out.println(f);
+//        double d = 3.14;
+
+        double value = 9.22 - 1.1;
+        double d = 3.32;
+
+        double res = value - d;
+        d--;
+//        System.out.println(res);
+    }
+}
