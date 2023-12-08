b8538af1c8347b190d8f1f6892f08566083b6b1b
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-12-08 15:10:18 -0600

    add java array practice

---
+++ Arrays/Arrays.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+        // String[] planets = new String[8];
+        // planets[0] = "Mercury";
+        // planets[1] = "Venus";
+        // planets[2] = "Earth";
+
+        // System.out.println(planets[0]);
+        // System.out.println(planets[5]);
+
+        // planets[3] = "Mars";
+        // planets[4] = "Jupiter";
+        // planets[5] = "Saturn";
+        // planets[6] = "Uranus";
+        // planets[7] = "Neptune";
+
+        // System.out.println(planets[2]);
+        // System.out.println(planets[5]);
+
+        String[] planets = { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" };
+        System.out.println(planets[1]);
+    }
+}
